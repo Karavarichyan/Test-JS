3366931a6990f9d45be6b733d220cd3b1e48411a
karavarichya <naerkkaravarichyan1910@mail.ru>
2024-05-23 13:03:44 +0300

    test map 20

--- 20/index.js
+++ 20/index.js
@@ -19,29 +19,48 @@
 //   }
 // console.log(stack.length);
 //   return stack.length === 0;
-  
+
 // };
 // console.log(isValid("(]][]){}[]"));
-/**
- * @param {string} s
- * @return {boolean}
- */
+
+// console.log(isValid("() [] {} AZ az")); // false
+// console.log(isValid("()[]{}")); // true
+// console.log(isValid("([{}])")); // true
+// console.log(isValid("({[})")); // false
+
+// /**
+//  * @param {string} s
+//  * @return {boolean}
+//  */
 // let isValid = function (s) {
 //   const stack = [];
-//   const map = {
-//     '(': ')',
-//     '[': ']',
-//     '{': '}'
-//   };
 
 //   for (let i = 0; i < s.length; i++) {
 //     const char = s.charAt(i);
 
-//     if (char === '(' || char === '[' || char === '{') {
+//     if (
+//       char === "(" ||
+//       char === "[" ||
+//       char === "{" ||
+//       char === "A" ||
+//       char === "a"
+//     ) {
 //       stack.push(char);
-//     } else if (char === ')' || char === ']' || char === '}') {
+//     } else if (
+//       char === ")" ||
+//       char === "]" ||
+//       char === "}" ||
+//       char === "Z" ||
+//       char === "z"
+//     ) {
 //       const last = stack.pop();
-//       if (map[last] !== char) {
+//       if (
+//         (char === ")" && last !== "(") ||
+//         (char === "]" && last !== "[") ||
+//         (char === "}" && last !== "{") ||
+//         (char === "Z" && last !== "A") ||
+//         (char === "z" && last !== "a")
+//       ) {
 //         return false;
 //       }
 //     }
@@ -50,36 +69,46 @@
 //   return stack.length === 0;
 // };
 
-// console.log(isValid("() [] {} AZ az")); // false
-// console.log(isValid("()[]{}")); // true
-// console.log(isValid("([{}])")); // true
-// console.log(isValid("({[})")); // false
+// console.log(isValid("(]][]){}[]")); // f
+// console.log(isValid("az()[]{}")); // t
+// console.log(isValid("az([{}])")); // t
+// console.log(isValid("az")); // fa
+// console.log(isValid("AZ")); // fa
+// console.log(isValid("aZ()[]{}")); // F
+
 /**
  * @param {string} s
  * @return {boolean}
  */
+
 let isValid = function (s) {
   const stack = [];
+
   
+  const charMap = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+    "A": "Z",
+    "a": "z",
+  };
+
   for (let i = 0; i < s.length; i++) {
     const char = s.charAt(i);
 
-    if (char === '(' || char === '[' || char === '{') {
+    if (charMap[char]) { 
       stack.push(char);
-    } else if (char === ')' || char === ']' || char === '}') {
-      const last = stack.pop();
-      if ((char === ')' && last !== '(') ||
-          (char === ']' && last !== '[') ||
-          (char === '}' && last !== '{')) {
-        return false;
-      }
+    } else if (charMap[stack.pop()] !== char) { 
+      return false;
     }
   }
 
-  return stack.length === 0;
+  return stack.length === 0; 
 };
 
-console.log(isValid("(]][]){}[]")); // f
-console.log(isValid("()[]{}")); // t
-console.log(isValid("([{}])")); // t
-console.log(isValid("az")); // fa
+console.log(isValid("(]][]){}[]")); // fal
+console.log(isValid("az()[]{}")); // tr
+console.log(isValid("az([{}])")); // tr
+console.log(isValid("az")); // tr
+console.log(isValid("Az")); // tr
+console.log(isValid("()[]{}")); // tr
